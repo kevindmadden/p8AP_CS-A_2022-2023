6445b1497404dc18f103b1f28812a695565b7abc
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2022-10-11 14:23:46 -0500

    Create s01_forLoops.java

---
+++ src/u04_IterationAndArrays/s01_forLoops.java
@@ -0,0 +1,26 @@
+package u04_IterationAndArrays;
+
+public class s01_forLoops {
+    public static void main(String[] args){
+
+        for(int i = 0; i<=10; i++){
+            System.out.println(i);
+        }
+
+        /*
+            1.) Variable i is created and assigned a value of 0.
+            2.) Checks middle condition to see if it is true or not.
+                If the condition is true, run the code between the two braces.
+                If the condition is false, it stops running.
+            3.) Print out a 0. (Running the code in the body of the for-loop)
+            4.) Runs i++;
+            5. ) Checks the middle condition to see if true. (1<=10 -> True statement.)
+            6. ) Print out a 1. (Running the code in the body of the for-loop)
+            7. ) Repeat until middle condition is no longer true.
+            8. ) Once that middle condition is false, then the code "jumps" to the close brace } of the for-loop and code ocnitnues running.
+
+         */
+
+
+    }
+}
