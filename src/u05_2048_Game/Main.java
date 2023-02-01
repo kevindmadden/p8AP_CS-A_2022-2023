0766293374acb27ac72ed266c3f8ca28c8a68f0a
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2023-02-01 14:14:09 -0600

    Assignment with the Code where the Main class looks like this. Block method still looks the same.
    -In Main Class
    -Call the constructor to create a new block in each slot of the 2d array
    
    Move more of the logic over to Block class from the Main class, including:
    	-Create a draw method inside the Block class. Move some of the logic for drawing an individual block over to the block class.
    	-There is a TODO with a comment below it in the Main class. You will need to expand the Block class in order to get this to work right without errors. To make it work right, you are going to need to make two new methods in the Block class: you should make a setNum and getNum so that you can set and get the num of a Block from the Main class.

--- src/u05_2048_Game/Main.java
+++ src/u05_2048_Game/Main.java
@@ -1,42 +1,9 @@
 package u05_2048_Game;
-
 /*
 M - Model (the inner workings of the logic)
 V - Drawing according to what is in the model
 C
-
  */
-
-
-/*
-Basic Outline
--Every time arrow is pressed, block between 2 and 4 spawns
---4 blocks spawn 10% of the time
--When arrow pressed, reshape the 2d array to that side
---Focus on what is added up to
---Remember to delete blocks
--If you press the arrow in a direction that changes nothing, then nothing happens
--If grid is completely full, then game is over if pressing the arrows does nothing.
--Game indicates to you somehow that you reached 2048 (confetti)
--Let's have holding down a key not work because my computer sucks
--Drawing
-    -Best way to implement a border
-        -Adjust scale in STD draw to not be 100x100
-            -60x60 scale with border in between of 5
-    -Draw with numbers centered
-    -Coloring - Change colors the larger they get (4096 is black)
-
-Structure
--UI for the arrows
--Method that checks
--Distinction between what grid looks like to user versus the actual state of the grid
--2d Array
- */
-
-// spawn first two blocks
-// -spawn on a single row (1 of 4 spots) and then get the left and right arrows working
-
-// thinkg about score
 
 
 import java.awt.*;
@@ -48,10 +15,7 @@ public class Main {
         StdDraw.setScale(0, 50);
         StdDraw.enableDoubleBuffering(); //Calling this method stops things from being drawn immediately after a draw method is called. This allows you to call many different draw methods without anything being drawn on the screen; when you call StdDraw.show(), everything will be drawn at once.
 
-        Block block = new Block();
-
-       // int[][] grid = new int[4][4];
-        Block[][] grid = new Block[4][4];
+        Block[][] blocks = new Block[4][4];
 
         boolean isLeftPressed = false;
 
@@ -59,16 +23,14 @@ public class Main {
         while (true) {
 
             if(!StdDraw.isKeyPressed(KeyEvent.VK_LEFT) &&  isLeftPressed){
-                grid=spawnNumber(grid);
+                blocks=spawnNumber(blocks);
                 isLeftPressed = false;
             }
 
             if(StdDraw.isKeyPressed(KeyEvent.VK_LEFT)){
                 isLeftPressed = true;
             }
-
-            //TODO: Fix this error below
-            //drawGrid(grid);
+            drawGrid(blocks);
 
 
 
@@ -78,51 +40,22 @@ public class Main {
         }
     }
 
-    public static Block[][] spawnNumber(Block[][] grid){
-        /*Method 1 - Non-lazy
-            Store the available cells that have 0's in an array.
-            Cycle through all elements in array and generate a list of cells where a # is not.
-            Generate a random number where there is a 0.
-         */
-        /*Method 2 - Lazy
-            Generate a random number.
-            If there is a 2 there, repeat previous step.
-         */
-
+    public static Block[][] spawnNumber(Block[][] blocks){
         int randomRow = 0;
         int randomCol = (int)(Math.random()*4);
         //TODO: Fix this error below when uncommented
-        /*while(grid[randomRow][randomCol] != 0){
+        /*while(blocks[randomRow][randomCol] != 0){
             randomRow = 0;
             randomCol = (int)(Math.random()*4);
         }
-        grid[randomRow][randomCol] = 2;*/
-        return grid;
+        blocks[randomRow][randomCol] = 2;*/
+        return blocks;
     }
 
 
-    public static void drawGrid(int[][] grid){
+    public static void drawGrid(Block[][] blocks){
         StdDraw.setPenColor(new Color(114, 83, 83));
         StdDraw.filledRectangle(25,25,25,25);
-
-        /*
-        --FIGURE OUT BASIC PATTERN FIRST, THEN CONVERT TO DOUBLE FOR-LOOP--
-        StdDraw.setPenColor(new Color(201, 60, 60));
-        StdDraw.filledSquare(7+12*0,7,5);
-        StdDraw.filledSquare(7+12*1,7,5);
-        StdDraw.filledSquare(7+12*2,7,5);
-        StdDraw.filledSquare(7+12*3,7,5);*/
-
-       /* for(int y=7; y<=7+12*grid.length; y+=12){
-            for(int x=7; x<=7+12*grid[0].length; x+=12){
-                //Draw the bg of the block
-                StdDraw.setPenColor(new Color(173, 125, 125));
-                StdDraw.filledSquare(x,y,5);
-
-
-            }
-        }*/
-
 
         for(int row=0; row<=3; row++){
             for(int col=0; col<=3; col++){
@@ -135,7 +68,7 @@ public class Main {
                 StdDraw.setPenColor(new Color(0, 0, 0));
                 Font font = new Font("Arial", Font.BOLD, 60);
                 StdDraw.setFont(font);
-                StdDraw.text(xCenter, yCenter, String.valueOf(grid[row][col]));
+                StdDraw.text(xCenter, yCenter, String.valueOf(blocks[row][col]));
             }
         }
 
